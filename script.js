be72df5e9fb2f155a36948b5675f4d4e016d3293
Frederick Banks <47834218+frederickbanks@users.noreply.github.com>
2019-04-17 17:35:54 -0400

    Add files via upload
    commit with html/css/js

---
+++ script.js
@@ -0,0 +1,45 @@
+let red = document.getElementById(red)
+let blue = document.getElementById(blue)
+let yellow = document.getElementById(yellow)
+let green = document.getElementById(green)
+
+// variable listed below were taken from example https://aiyentam.github.io/Simon-Says/
+var simonSaysArray = [];
+var playerArray = [];
+var score = 0;
+var currentRound = 0;
+var sequence = 0
+var started = false
+var quit = false
+
+// end of used code
+
+
+var simonSays = {
+    colors = [red,blue,green,yellow],
+    sound = {
+        blue: new Audio (`https://s3.amazonaws.com/freecodecamp/simonSound1.mp3`),
+        red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
+        yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
+        green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+// const game = {
+    
+
+
+
+
+// }
