539bed347d45cb8a16c7289c4de63bab96b9dba9
Frederick Banks <frederickbanks@icloud.com>
2020-01-13 21:41:37 -0500

    cleaning up files

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let red = document.getElementById(`colorRed`)
-let blue = document.getElementById(`colorBlue`)
-let yellow = document.getElementById(`colorYellow`)
-let green = document.getElementById(`colorGreen`)
+let red = document.getElementById(`colorRed`);
+let blue = document.getElementById(`colorBlue`);
+let yellow = document.getElementById(`colorYellow`);
+let green = document.getElementById(`colorGreen`);
 // let start = document.getElementById(`start`)
 
-var start = document.querySelector('#start')
+var start = document.querySelector("#start");
 
 var simonSaysArray = [];
 var playerArray = [];
@@ -16,128 +16,85 @@ var colors = [`red`, `blue`, `green`, `yellow`];
 // variable listed below were taken from example https://aiyentam.github.io/Simon-Says/
 
 var simonSays = {
-    started: false,
-    quit: false,
-    // end of used code
-    sound: {
-        blue: new Audio(`https://s3.amazonaws.com/freecodecamp/simonSound1.mp3`),
-        red: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3'),
-        yellow: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3'),
-        green: new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'),
-    }
-}
+  started: false,
+  quit: false,
+  // end of used code
+  sound: {
+    blue: new Audio(`https://s3.amazonaws.com/freecodecamp/simonSound1.mp3`),
+    red: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
+    yellow: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
+    green: new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3")
+  }
+};
 console.log(simonSays);
 
 // var startCycle = function (arr) {
 //     return arr.push(simonSays.colors[Math.floor(Math.random() * 4)])
 // }
 
-
-
-
-start.addEventListener('click', handleStart)
+start.addEventListener("click", handleStart);
 
 function handleStart(evt) {
-    evt.preventDefault()
-    console.log('button is working');
-    // Push random color(s)
+  evt.preventDefault();
+  console.log("button is working");
+  // Push random color(s)
 
-    simonSaysArray.push(colors[Math.floor(Math.random() * 4)])
-    for(let i = 0; i <= simonSaysArray.length; i++){  
-        
+  simonSaysArray.push(colors[Math.floor(Math.random() * 4)]);
+  for (let i = 0; i <= simonSaysArray.length; i++) {}
+
+  console.log(simonSaysArray);
+}
+
+red.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  console.log("red is working");
+  playerArray.push("red");
+  console.log(playerArray);
+});
+
+blue.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  console.log("blue is working");
+  playerArray.push("blue");
+  console.log(playerArray);
+});
+
+yellow.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  console.log("yellow is working");
+  playerArray.push("yellow");
+  console.log(playerArray);
+});
+
+green.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  console.log("green is working");
+  playerArray.push("green");
+  console.log(playerArray);
+});
+
+function checkColors() {
+  for (let i = 0; i < simonSaysArray.length; i++) {
+    if (simonSaysArray[i] === playerArray[i]) {
+      console.log("correct");
+      playerArray = [];
+      handleStart();
+    } else {
+      console.log("incorrect");
+      clearGame();
     }
-
-    console.log(simonSaysArray);
-    
+  }
 }
-
-
-red.addEventListener('click', function (evt) {
-
-    evt.preventDefault()
-    console.log('red is working');
-    playerArray.push('red');
-    console.log(playerArray);
-
-}
-)
-
-blue.addEventListener('click', function (evt) {
-
-    evt.preventDefault()
-    console.log('blue is working');
-    playerArray.push('blue');
-    console.log(playerArray);
-}
-)
-
-yellow.addEventListener('click', function (evt) {
-
-    evt.preventDefault()
-    console.log('yellow is working');
-    playerArray.push('yellow');
-    console.log(playerArray);
-}
-)
-
-green.addEventListener('click', function (evt) {
-
-    evt.preventDefault()
-    console.log('green is working');
-    playerArray.push('green');
-    console.log(playerArray);
-    
-}
-)
-
-function checkColors(){
-    for(let i = 0; i < simonSaysArray.length; i++) {
-        if (simonSaysArray[i] === playerArray[i]){
-            console.log('correct');
-            playerArray = []
-            handleStart();
-            
-        }
-        else {
-            console.log('incorrect');
-            clearGame();
-            
-            
-        }
-        
-    }
-        
-    }
-
-
-
-
-
-
-
 
 // function startCycle(arr) {
 
 //     return arr.push(simonSays.colors)
 // }
 
-
-// function startSimonTimer() {
-//     var i = 0;
-//     var setTimer = setInterval(function () {
-//         $('#color' + simonSays.simonSaysArray[i]).fadeTo('slow', 0).fadeTo('slow', 1); i++;
-//         if (i >= simonSays.simonSaysArray.length) {
-//             clearInterval(setTimer);
-//         }
-
-//     }, 1500);
-// };
-
-
 function clearGame() {
-        currentRound = 0
-        simonSaysArray = []
-        playerArray = []
+  currentRound = 0;
+  simonSaysArray = [];
+  playerArray = [];
 }
 
 // function clearAnswers() {
@@ -155,7 +112,6 @@ function clearGame() {
 //     simonSays.simonSaysArray = [];
 
 // }
-
 
 // function startGame() {
 //     if (simonSays.currentRound === 10) {
@@ -184,17 +140,9 @@ function clearGame() {
 //             alert ('Sorry, you lose');
 //             resetSimon();
 //         }
-    
 
 // }
 
-
-
-
 // }
-
-
-
 
 // );
-
